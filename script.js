f72c847d7ab9ca0a08cacd74b672fbf1ab7692a8
Mike Mitchell <mmitchell8910@gmail.com>
2022-09-27 15:33:31 -0600

    Added a timer reset button

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-const startingMinutes = 25;
+const countdownEl = document.getElementsByClassName('countdown')[0];
+const startBtn = document.getElementsByClassName('timerStart')[0];
+
+let startingMinutes = 25;
 let time = startingMinutes * 60;
 
-const countdownEl = document.getElementById('countdown');
+function updateCountdown() {
+  if (time >= 1) {
+    const minutes = Math.floor(time / 60);
+    let seconds = time % 60;
+
+    seconds = seconds < 10 ? '0' + seconds : seconds;
+
+    countdownEl.innerHTML = `${minutes}:${seconds}`;
+    time--;
+  } else {
+    countdownEl.innerHTML = "DONE!";
+  }
+}
 
 setInterval(updateCountdown, 1000);
 
-function updateCountdown() {
-    if (time >= 1) {
-        const minutes = Math.floor(time / 60);
-        let seconds = time % 60;
-
-        seconds = seconds < 10 ? '0' + seconds : seconds;
-
-        countdownEl.innerHTML = `${minutes}:${seconds}`;
-        time--;
-    } else {
-        countdownEl.innerHTML = "DONE!";
-    }
-}
+startBtn.addEventListener("click", () => {
+  window.location.reload();
+});
